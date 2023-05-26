70204245a33c5297639f7b1ea1ee0e2efb4142b8
Adrien Dessilly <adessilly@gmail.com>
2023-05-27 00:32:16 +0200

    initial commit

---
+++ src/test/java/com/example/testmail/TestmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
